d0b7c85729e9ae29c45b1f9ce918319a87cd0490
friedrich420 <kelpidorou@yahoo.com>
2015-01-31 09:32:52 +0200

    fix various compilation warnings

--- drivers/cpufreq/cpufreq_yankactive.c
+++ drivers/cpufreq/cpufreq_yankactive.c
@@ -834,7 +834,7 @@ static ssize_t show_target_loads(
 		ret += sprintf(buf + ret, "%u%s", target_loads[i],
 			       i & 0x1 ? ":" : " ");
 
-	ret += sprintf(buf + --ret, "\n");
+	ret += sprintf(buf + ret, "\n");
 	spin_unlock_irqrestore(&target_loads_lock, flags);
 	return ret;
 }
@@ -877,7 +877,7 @@ static ssize_t show_above_hispeed_delay(
 		ret += sprintf(buf + ret, "%u%s", above_hispeed_delay[i],
 			       i & 0x1 ? ":" : " ");
 
-	ret += sprintf(buf + --ret, "\n");
+	ret += sprintf(buf + ret, "\n");
 	spin_unlock_irqrestore(&above_hispeed_delay_lock, flags);
 	return ret;
 }
