87a6651333e73d2cf95e7ff3b9731cc9f4017075
friedrich420 <kelpidorou@yahoo.com>
2015-03-21 15:10:17 +0200

    powersuspend: fixes

--- kernel/power/autosleep.c
+++ kernel/power/autosleep.c
@@ -122,19 +122,8 @@ int pm_autosleep_set_state(suspend_state_t state)
 	if (state > PM_SUSPEND_ON) {
 		pm_wakep_autosleep_enabled(true);
 		queue_up_suspend_work();
-<<<<<<< HEAD
-#ifdef CONFIG_POWERSUSPEND
-		set_power_suspend_state_autosleep_hook(POWER_SUSPEND_ACTIVE); // Yank555.lu : add hook to handle powersuspend tasks
-#endif
 	} else {
 		pm_wakep_autosleep_enabled(false);
-#ifdef CONFIG_POWERSUSPEND
-		set_power_suspend_state_autosleep_hook(POWER_SUSPEND_INACTIVE); // Yank555.lu : add hook to handle powersuspend tasks
-#endif
-=======
-	} else {
-		pm_wakep_autosleep_enabled(false);
->>>>>>> 5e581e8... kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend v1.7 (faux123/Yank555.lu)
 	}
 
 	mutex_unlock(&autosleep_lock);
