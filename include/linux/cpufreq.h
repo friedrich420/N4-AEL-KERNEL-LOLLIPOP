3ac4ed0c2f0f58c32185076af2f5d06dc1716588
friedrich420 <kelpidorou@yahoo.com>
2015-04-20 19:04:06 +0300

    Added mythx_plug CPU governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -531,6 +531,9 @@ extern struct cpufreq_governor cpufreq_gov_optimax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PRESERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_preservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_preservative)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_MYTHX_PLUG)
+extern struct cpufreq_governor cpufreq_gov_myhtx_plug;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_myhtx_plug)
 #endif
 
 /*********************************************************************
